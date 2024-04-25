2af4f7302561a890bd359229b417e754dc795ca2
Florian König <florian.koenig@smake.com>
2024-04-25 07:21:12 +0200

    Add cloak Disable Arrow Keys

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -207,4 +207,13 @@ return {
       },
     },
   },
+  {
+    'laytan/cloak.nvim',
+    config = function()
+      require('cloak').setup {
+        enabled = true,
+        cloak_length = 32,
+      }
+    end,
+  },
 }
